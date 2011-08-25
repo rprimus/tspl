1fba431be32deaf3c883d110644f38aeb0825c30
R Primus <rprimus+github@gmail.com>
2011-08-25 21:03:27 +0100

    start on ex2.2.2

---
+++ chap2/ex2.2.2.scm
@@ -0,0 +1,5 @@
+;; Thu Aug 25 20:55:34 BST 2011 -primus
+;; number classes in scheme: integers, rational numbers, real numbers, or complex numbers
+;; eg: (123456 12/5 234.56 2.3456e+2 1/2+9i 1.1@-1/2)
+
+
