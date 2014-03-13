db15fff506e755ad6de260b5d8edd25efd64d81a
Richard Primus <rprimus+github@gmail.com>
2014-03-13 13:42:30 +0000

    add test file

---
+++ tt.scm
@@ -0,0 +1,25 @@
+;; This buffer is for notes you don't want to save, and for Lisp evaluation.
+;; If you want to create a file, visit that file with C-x C-f,
+;; then enter the text in that file's own buffer.
+
+(define recip2
+  (lambda (n)
+    (if (= n 0)
+        "opps!"
+        (/ 1 n))))
+
+
+((lambda (n)
+    (+ n 5)) 4)
+
+;;; sweet
+(a b  a)
+
+
+
+(conj [1 2 3] 4)
+
+(eat (cky) bcn)
+
+(omg (eat [cky]) bcn)
+(important)
